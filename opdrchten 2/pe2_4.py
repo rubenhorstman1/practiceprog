fa684c36827dae27540999360e605df9e680a97c
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:26:14 +0200

    pe2_4

---
+++ opdrchten 2/pe2_4.py
@@ -0,0 +1,4 @@
+naam = float(input('Wat verdien je per uur: '))
+verdient = float(input('Hoeveel uur heb je gewerkt: '))
+totaal = (naam*verdient)
+print(str(verdient) + ' ' + 'uur werken levert' + ' '+str(totaal)+ ' '+ 'euro op')
