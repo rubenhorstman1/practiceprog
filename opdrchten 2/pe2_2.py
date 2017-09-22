2552abf6ddbfa5ba3f4219f2e0bc1ab7dc2b4131
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:25:52 +0200

    pe2_2

---
+++ opdrchten 2/pe2_2.py
@@ -0,0 +1,9 @@
+cijferICOR = 8
+cijferPROG = 7
+cijferCSN = 6
+totaal = cijferICOR+ cijferPROG + cijferCSN
+gem = str(totaal / 3)
+beloning = str(totaal*30)
+print('mijn cijfers' + ' ' + gem + ' ' + 'leveren een totaalbedrag op van' + ' ' + beloning)
+
+
