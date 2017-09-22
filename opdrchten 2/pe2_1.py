1e414734f919d75bef8bf3ab9b2872c950884bb1
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:25:35 +0200

    pe2_1

---
+++ opdrchten 2/pe2_1.py
@@ -0,0 +1,9 @@
+getal = [2,5,7,9,10,22,55,43,99]
+for count in getal:
+    if count%2==0:
+        print(count)
+
+list = ['A, A, B, C, B, B, C, C, C']
+list.sort()
+lijst= (list.count('A'), list.count('B'), list.count('C'))
+print(lijst)
