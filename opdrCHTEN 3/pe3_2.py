f7eecf7c59610c4a1a425d366fc3b38c140361ac
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:24:35 +0200

    pe3_2

---
+++ opdrCHTEN 3/pe3_2.py
@@ -0,0 +1,13 @@
+s = "Guido van Rossum heeft programmeertaal Python bedacht."
+letters = ['a', 'e', 'u', 'i', 'o']
+for count in s:
+    if count in letters:
+     print(count)
+Gefeliciteerd, je mag stemmen!
+leeftijd = input('Geef je leeftijd: ')
+paspoort = (input('Nederlands paspoort: '))
+antwoord = (ja)
+if paspoort = antwoord
+    print('gefeliciteerd, je mag stemmen')
+else:
+    print('je bent bronze')
