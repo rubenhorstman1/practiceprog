d4c6291bf69f21f127639f9fe9282f11b5e94629
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:24:44 +0200

    pe3_3

---
+++ opdrCHTEN 3/pe3_3.py
@@ -0,0 +1,7 @@
+leeftijd = input('Geef je leeftijd: ')
+paspoort = (input('Nederlands paspoort: '))
+antwoord = ('ja')
+if paspoort = antwoord
+    print('gefeliciteerd, je mag stemmen')
+else:
+    print('je mag niet stemmen’)
