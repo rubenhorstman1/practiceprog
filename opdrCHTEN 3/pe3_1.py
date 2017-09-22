4325b6e15362b98147d43cc39836c415cf0db0ed
ruben <ruben.horstman@student.hu.nl>
2017-09-22 10:24:21 +0200

    pe3_1

---
+++ opdrCHTEN 3/pe3_1.py
@@ -0,0 +1,8 @@
+
+        score = input('geef je score: ')
+        voldoende = 15
+        if int(score) >= voldoende:
+            print('gefeliciteerd!')
+            print('Met deze score ben je geslaagd')
+        else:
+            print('met deze score ben je niet geslaagd')
